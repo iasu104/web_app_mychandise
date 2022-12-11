2891768ce844990658479b58f126b78db668a079
Ubuntu <ubuntu@ip-172-31-12-214.ap-northeast-1.compute.internal>
2022-12-11 05:52:22 +0000

    Initialize Repository

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2c257a75ff76de351abe0019276e049d5c43f920ba42d10d724ab2676d625896ced0ae564f5ab94f22c7f9af0534b1ab6a83aff6fc101b0a6ec0d1669b3c4a7e'
